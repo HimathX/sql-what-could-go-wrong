0e2b02430f16f9b21ef9734756071c24e7787d4d
Himath Nimpura <himathd.23@cse.mrt.ac.lk>
2025-12-27 17:20:13 +0530

    Load DB config from .env and refactor connection
    Replaces hardcoded Supabase Postgres connection details with environment variable loading via dotenv. Updates the connection string to use MIGRATION_URI from the environment, improving security and flexibility.

--- migration.py
+++ migration.py
@@ -2,29 +2,22 @@ import sqlite3
 import pandas as pd
 from sqlalchemy import create_engine
 import urllib.parse
+from dotenv import load_dotenv
+import os
 
-# --- CONFIGURATION ---
-SQLITE_DB_PATH = 'Chinook.db'
 
-# 1. NEW POOLER DETAILS (Found in Supabase Dashboard -> Connect)
-# Note the host ends in .pooler.supabase.com
-# Note the user format: "postgres.[your-project-ref]"
-user = "postgres.xaktpivqlivofxbrcubi"  # Add your project ref after a dot
-password = "Adminuser!234"
-host = "aws-0-us-east-1.pooler.supabase.com" # Check your dashboard for your specific region
-port = "5432" # 5432 for Session mode (recommended for migrations), 6543 for Transaction mode
-database = "postgres"
-
-safe_password = urllib.parse.quote_plus(password)
+load_dotenv()
 
 # Final Connection String
-SUPABASE_POSTGRES_URL = "postgresql://postgres.xaktpivqlivofxbrcubi:Adminuser!234@aws-1-ap-south-1.pooler.supabase.com:5432/postgres"
+MIGRATION_URL = os.getenv("MIGRATION_URI")
+
+SQLITE_DB_PATH = "./Chinook.db"
 
 def migrate():
     try:
         sqlite_conn = sqlite3.connect(SQLITE_DB_PATH)
         # Use a small connect_timeout to catch errors faster
-        pg_engine = create_engine(SUPABASE_POSTGRES_URL, connect_args={'connect_timeout': 10})
+        pg_engine = create_engine(MIGRATION_URL , connect_args={'connect_timeout': 10})
 
         tables = pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table';", sqlite_conn)
         
